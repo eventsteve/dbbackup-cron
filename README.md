7280e369a5bf575b7a59a4863886ee382888949c
vahit <vahid.maani@gmail.com>
2018-01-14 15:01:15 +0330

    some changes ...

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # DBBackup cron
 ## A kubernetes cronjob to take backup from the DB and upload it on the backup server.
 ### ENVs:
-+ **PUBLIC_KEY**: ssh rsa public key.
-+ **PRIVATE_KEY**: ssh rsa private key.
 + **DB_USER**: DataBase username.
 + **DB_PASS**: DataBase password.
 + **DB_HOST**: DataBase hostname.
 + **DB_NAME**: DataBase name.
 + **BACKUP_SERVER**: Backup destication server.
-+ **REMOTE_DIR**: Directory on Backup server.
++ **REMOTE_FILE**: Backup file name and directory on Backup server.
++ **FILE_NAME**: Local file name.
+
+
+
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,17 +2,9 @@
 
 set -eo pipefail
 
-# make sshd needed directories
-# mkdir -p /root/.ssh
 mkdir -p /var/run/sshd
-
-# add ssh private and public keys to root user
-# echo "${PUBLIC_KEY}" | tee -a /root/.ssh/id_rsa.pub
-# echo "${PRIVATE_KEY}" | tee -a /root/.ssh/id_rsa
-# chmod 600 /root/.ssh/id_rsa*
-
 chmod 600 -R /root/.ssh
 
-bash -x /root/db-backuper.sh -u "${DB_USER}" -p "${DB_PASS}" -h "${DB_HOST}" -d "${DB_NAME}" -l /root/${FILE_NAME}.sql -r /backup1/${REMOTE_DIR} -b ${BACKUP_SERVER}
+bash -x /root/db-backuper.sh -u "${DB_USER}" -p "${DB_PASS}" -h "${DB_HOST}" -d "${DB_NAME}" -l /root/"${FILE_NAME}".sql -r /"${REMOTE_FILE}" -b "${BACKUP_SERVER}"
 
 exit 0
